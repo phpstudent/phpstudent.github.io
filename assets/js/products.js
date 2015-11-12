df5618b0898c1bdc05cffbffc81744784496ca7e
phpstudent <mazhartejani@gmail.com>
2015-11-12 14:07:49 +0400

    .

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+		//highlight and scroll to active sidenav link
+		var clicked = $("div#sideNav nav ul li a, ul.mainLinks li ul.sublinks li a");
+		clicked.click(function(event){
+
+			clicked.removeClass("active"); //remove active class from all
+			$(this).addClass("active"); //add active class to select
+			event.preventDefault();
+    		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+		});
+
+		//fixed side nav bar after 300 pixels scrolling
 		$(window).scroll(function(){
 				var bodyscroll = $("body").scrollTop();
 			if(bodyscroll > 300){
